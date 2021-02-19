b97146bf48cbada04dcd5af418a0a45f9279596e
Danimjs <77038782+Danimjs@users.noreply.github.com>
2021-02-19 14:06:45 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default class Config  {
-    mapboxKey = "pk.eyJ1IjoiZGFuaW1qcyIsImEiOiJja2xib2Nscncwc2gzMnhueGpyNmY0d3h3In0._PacZq2T6eoYW7VxCzAkpQ"
+    mapboxKey = "pk.eyJ1IjoiZGFuaW1qcyIsImEiOiJja2xicG9oMDUzMW5vMnFxanBxaHlkbDU4In0.DK3TRba9aY2etY91I2wI1A"
 }
