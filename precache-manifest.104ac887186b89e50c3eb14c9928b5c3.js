212330183508e59d7a3270666ead802583bfa9e8
mcyph <20507948+mcyph@users.noreply.github.com>
2021-02-26 17:07:04 +1100

    Updates

---
+++ precache-manifest.104ac887186b89e50c3eb14c9928b5c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8418ca07e1189f086f910dc3ffc130",
+    "url": "/aussieElectro/index.html"
+  },
+  {
+    "revision": "bfb941588ed8a634f76e",
+    "url": "/aussieElectro/static/css/main.1438a198.chunk.css"
+  },
+  {
+    "revision": "57fcf079cc686e4995e8",
+    "url": "/aussieElectro/static/js/2.5c463ac7.chunk.js"
+  },
+  {
+    "revision": "66a3db79a39a11283737dfbd3097f0c2",
+    "url": "/aussieElectro/static/js/2.5c463ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb941588ed8a634f76e",
+    "url": "/aussieElectro/static/js/main.d7fd5af3.chunk.js"
+  },
+  {
+    "revision": "626bcb83cad3c2c20fff",
+    "url": "/aussieElectro/static/js/runtime-main.24f36cbe.js"
+  },
+  {
+    "revision": "713f33cea62b0bd949d1e5fc7bd4fb41",
+    "url": "/aussieElectro/static/media/yearDistribution.713f33ce.csv"
+  }
+]);
